427106c750e11dec3294a56af632ea9754298b8f
zasquash <bardon.jeanalexandre@gmail.com>
2016-12-21 20:18:26 +0100

    merge method serial interface

--- server/config.py
+++ server/config.py
@@ -5,10 +5,10 @@ socketBind = "0.0.0.0"
 socketPort = 8000
 
 # Serial interface
+# Enable serial interface
+isSerial = False
+
 # example for one serial port : portCom = [{ 'port' : '/dev/ttyUSB0', 'baudrate': 115200 }]
 # example for n serial port : portCom = [{ 'port': '/dev/ttyUSB0', 'baudrate': 115200 }, { 'port':'/dev/ttyUSB1', 'baudrate': 115200 }]
 # example for n serial port for windows: portCom = [{ 'port': 'COM1', 'baudrate': 115200 }, {'port': 'COM2', 'baudrate': 115200 }]
 portCom = [{ 'port' : '/dev/ttyUSB0', 'baudrate': 115200 },{ 'port' : '/dev/ttyS0', 'baudrate': 115200 }]
-bytesize = 8
-parity = "None"
-stopbits = 1
